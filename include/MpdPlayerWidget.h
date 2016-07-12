2dbe3e07cb5849cddf938a6740e59d82faf98443
Thomas Zimmermann <bugs@vdm-design.de>
2016-07-12 21:00:01 +0200

    Add missing MpdPlayerWidet.h, remove some unneded files

---
+++ include/MpdPlayerWidget.h
@@ -0,0 +1,33 @@
+#ifndef MPDPLAYERWIDGET_H
+#define MPDPLAYERWIDGET_H
+
+#include <QDeclarativeView>
+#include <QDeclarativeProperty>
+
+#include <QMpdClient>
+
+class MpdPlayerWidget : public QDeclarativeView {
+	Q_OBJECT
+
+public:
+	MpdPlayerWidget();
+	~MpdPlayerWidget();
+
+private:
+	QMpdClient *mpd;
+	QString currentPath;
+	QDeclarativeProperty *qmlState;
+	bool connected;
+
+private slots:
+	void onMpdInitialized();
+	void setVolume(int v);
+	void stateChanged();
+	void populateMenu();
+	void onMenuItemSelected(QString key, QString name);
+	void onSongChanged(const QMpdSong& song);
+	void onMpdStatusChanged(const QMpdStatus::State state);
+	void onPlayStopClicked();
+};
+
+#endif // MPDPLAYERWIDGET_H
